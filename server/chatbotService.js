971abed4d8acd7d3b0093370c55d0e84af201702
Aaron McKie <aaron.mckie@icloud.com>
2021-07-01 15:50:25 +0100

    Adding some NLP/NLG training with some basic responses added in server

---
+++ server/chatbotService.js
@@ -0,0 +1,48 @@
+const { NlpManager } = require('node-nlp');
+const manager = new NlpManager({ languages: ['en'] });
+
+//Training the AI
+async function trainChatBotAI() {
+    return new Promise(async (resolve, reject) => {
+        //Adds intents to Natural Language Processor
+        //Also trains Natural Lamguage Generator
+        manager.addDocument('en', 'Tatty-bye!', 'greetings.goodbye');
+        manager.addDocument('en', 'Farewell!', 'greetings.goodbye');
+        manager.addDocument('en', 'So Long!', 'greetings.goodbye');
+        manager.addDocument('en', 'Hello there!', 'greetings.hello');
+        manager.addDocument('en', 'Nice to see you!', 'greetings.hello');
+        manager.addDocument('en', 'Howdy!', 'greetings.hello');
+
+        //Training for NLG
+        manager.addAnswer('en', 'greetings.goodbye', 'Until next time!');
+        manager.addAnswer('en', 'greetings.goodbye', 'See you soon!');
+        manager.addAnswer('en', 'greetings.hello', 'Hello!');
+        manager.addAnswer('en', 'greetings.hello', 'Nice to see you!');
+
+await.manager.train();
+    manager.save();
+    console.log("The AI has been trained!")
+    resolve(true);
+    })
+}
+
+async function generateResponseAI(qsm) {
+    //Train and save model
+    return new Promise(async(resolve, reject) => {
+        response = await manager.process('en', qsm);
+        resolve(response);
+    })
+}
+
+const connectWebSocket = (io) => {
+    io.on('connection', function(socket) {
+        socket.on('join', (userId) => {
+            socket.join(userId);
+            console.log("New user has joined the room")
+        });
+
+socket.on('new-msg', async function (data) {
+
+})
+    })
+}
